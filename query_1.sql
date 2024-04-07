d59139f166f50c9e794262db5efe6567684c03e4
ngzxzxzx <63797997+ngzxzxzx@users.noreply.github.com>
2024-04-07 15:23:33 +0800

    Update query_1.sql

--- query_1.sql
+++ query_1.sql
@@ -1,9 +1,15 @@
+/* join day_package and related tables based on the matching user_id*/
 WITH PackageRelations AS (
     SELECT dp.id, r.user_1_id, r.user_2_id, r.type
     FROM day_package dp
     INNER JOIN related r ON dp.user_id = r.user_1_id OR dp.user_id = r.user_2_id
 ),
-    
+
+/* join day_package with PackageRelations with matching package id*/
+/* Select the package name (description) and the user counts of package*/
+/* participant_count is the count where all users of that package are either family or club*/
+/* family_count is the count where all users of that package are related by family*/
+/* club_count is the count where all users of that package are related by club*/
 PackageCounts AS (
     SELECT dp.description, 
            COUNT(DISTINCT CASE WHEN pr.type IN ('Family', 'Club') THEN pr.user_1_id END) AS participant_count,
@@ -13,7 +19,8 @@ PackageCounts AS (
     JOIN PackageRelations pr ON dp.id = pr.id
     GROUP BY dp.description
 ),
-    
+
+/* Select only packages where pc.family_count = pc.participant_count OR pc.club_count = pc.participant_count*/
 FilteredPackages AS (
     SELECT pc.description, 
            pc.participant_count AS popularity,

a9ee491a1a78481f0896ad1135909a86e8f8bd5f
Isaeo <isaacisyeo@gmail.com>
2024-04-09 06:43:18 +0800

    Update test_query_6.sql

--- test_query_6.sql
+++ test_query_6.sql
@@ -1,31 +1,27 @@
--- First result set: Top 3 Malls
-SELECT TOP 3
-  m.address AS mall_address,
-  SUM(st.amount_spent) AS total_mall_shop_earnings,
-  SUM(rt.amount_spent) AS total_mall_restaurant_earnings,
-  SUM(st.amount_spent) + SUM(rt.amount_spent) AS total_mall_earnings,
-  'Mall' AS entity_type
-FROM
-  mall m
-  JOIN shop s ON m.id = s.mall_id
-  JOIN shop_transaction st ON s.id = st.shop_id
+WITH shop_earnings AS (
+  SELECT 
+    m.address AS mall_address,
+    SUM(st.amount_spent) AS total_mall_shop_earnings
+  FROM mall m
+  INNER JOIN shop s ON m.id = s.mall_id
+  INNER JOIN shop_transaction st ON s.id = st.shop_id
+  GROUP BY m.address
+),
+restaurant_earnings AS (
+  SELECT
+    m.address AS mall_address,
+    SUM(COALESCE(rt.amount_spent, 0)) AS total_mall_restaurant_earnings
+  FROM mall m
   LEFT JOIN restaurant_outlet ro ON m.id = ro.mall_id
   LEFT JOIN restaurant_transaction rt ON ro.id = rt.restaurant_outlet_id
-GROUP BY
-  m.address
-ORDER BY
-  total_mall_earnings DESC;
-
--- Second result set: Top 3 Restaurants
-SELECT TOP 3 
-  rc.address AS restaurant_chain_address, 
-  SUM(rt.amount_spent) AS total_restaurant_earnings, 
-  'Restaurant' AS entity_type
-FROM 
-  restaurant_chain rc
-  JOIN restaurant_outlet ro ON rc.id = ro.restaurant_id
-  JOIN restaurant_transaction rt ON ro.id = rt.restaurant_outlet_id
-GROUP BY 
-  rc.address
-ORDER BY 
-  SUM(rt.amount_spent) DESC;
+  GROUP BY m.address
+)
+SELECT TOP 3
+  se.mall_address,
+  se.total_mall_shop_earnings,
+  re.total_mall_restaurant_earnings,
+  se.total_mall_shop_earnings + COALESCE(re.total_mall_restaurant_earnings, 0) AS total_mall_earnings,
+  'Mall' AS entity_type
+FROM shop_earnings se
+LEFT JOIN restaurant_earnings re ON se.mall_address = re.mall_address
+ORDER BY se.total_mall_shop_earnings DESC;
